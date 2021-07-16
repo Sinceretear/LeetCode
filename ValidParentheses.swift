4a972125ed3f5ca1af3f70d060d2346e1ab8944d
Hunter <bobnamed123@yahoo.com>
2021-07-16 16:57:33 -0700

    Create ValidParentheses.swift

---
+++ ValidParentheses.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        
+        if s.count % 2 != 0 { return false }
+        
+        var stack: [Character] = []
+     
+        for char in s {
+            switch char {
+               case "(":
+                    stack.append(")")
+                case "[":
+                    stack.append("]")
+                case "{":
+                    stack.append("}")
+                default:
+                    if stack.isEmpty || stack.removeLast() != char {
+                        return false
+                    }
+            }
+        }
+        return stack.isEmpty
+        
+    }
+}
+
+//need more practice with this one
