920667788012d0cfd80863c64422edeac95191fc
Hunter <bobnamed123@yahoo.com>
2021-09-09 10:10:50 -0700

    Create BinarySearch.swift

---
+++ BinarySearch.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left  = 0
+        var right = nums.count - 1
+        
+        while (left <= right) {
+             var pivot = left + (right - left) / 2;
+                if(nums[pivot] == target) { return pivot}
+                if (target < nums[pivot])  {
+                    right = pivot - 1
+                } else {
+                    left = pivot + 1
+                }
+        }
+        return -1
+    }
+}
