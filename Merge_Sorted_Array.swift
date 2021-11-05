1dd01e1ed00f6d6ea96759544ab21c4f6a62405f
Hunter <bobnamed123@yahoo.com>
2021-11-04 19:49:32 -0700

    Update Merge_Sorted_Array.swift

--- Merge_Sorted_Array.swift
+++ Merge_Sorted_Array.swift
@@ -42,3 +42,30 @@ var b = [8,9,10,11]
 var sms = mergeSortedArrays(arr1: &a, arr2: &b)
 
 print(sms)
+
+
+
+Still don't under stand this problem but this is my second try
+
+class Solution {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        
+        var m = m
+        var n = n
+        
+           while m > 0 && n > 0 {
+               if nums1[m-1] > nums2[n-1]{
+                   nums1[m+n-1] = nums1[m-1]
+                   m = m - 1
+               } else {
+                   nums1[m+n-1] = nums2[n-1]
+                   n = n - 1
+               }
+           }
+            while n > 0 {
+                nums1[m+n-1] = nums2[n-1]
+                n = n - 1
+            }
+    }
+}
+
