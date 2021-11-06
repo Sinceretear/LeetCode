16d5d473064616edc81778d50e51704a83265494
Hunter <bobnamed123@yahoo.com>
2021-11-06 16:52:33 -0700

    Create Search_Insert_Position_#35.swift

---
+++ Search_Insert_Position_#35.swift
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/search-insert-position/
+
+
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        
+        var l  = 0
+        var r = nums.count - 1
+        
+        while (l <= r) {
+            
+            var mid = l + ( r - l) / 2 
+            
+            if nums[mid] == target {
+                return mid 
+            } else if nums[mid] < target {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }  
+        }
+        return l
+        
+    }
+}
