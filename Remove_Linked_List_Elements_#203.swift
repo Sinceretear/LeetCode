88f47f5485ebe96bfe6ec8fe474842e4f2e66e0b
Hunter <bobnamed123@yahoo.com>
2021-11-09 14:30:18 -0800

    Create Remove_Linked_List_Elements_#203.swift

---
+++ Remove_Linked_List_Elements_#203.swift
@@ -0,0 +1,51 @@
+203. Remove Linked List Elements
+
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+        
+        var dummy = ListNode()
+        var prev: ListNode? = dummy
+        var current = head
+        
+        while let c = current {
+            if c.val != val {
+                prev?.next = c
+                prev = c
+            } else {
+                prev?.next = nil
+            }
+            current = c.next
+        }
+        return dummy.next
+        
+        
+    }
+}
